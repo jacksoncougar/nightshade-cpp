5e6ae64b6d2b6d6fcc746922b8fedf78e78432ae
jacksoncougar <jacksoncougar@gmail.com>
2020-05-09 12:37:35 -0600

    refactoring/cleanup

---
+++ nightshade/logging.h
@@ -0,0 +1,95 @@
+
+#include <string>
+
+#include <iomanip>
+#include <iostream>
+
+namespace std
+{
+std::string to_string(WCHAR *str)
+{
+  std::wstring ws(str);
+  return std::string(ws.begin(), ws.end());
+}
+
+std::string to_string(const char *str) { return std::string(str); }
+} // namespace std
+
+namespace ns
+{
+template <typename... T> void log(T... x)
+{
+  std::ostringstream ss;
+  time_t t = std::time(0);
+  ss << std::setw(10) << std::put_time(std::localtime(&t), "%H-%M-%S: ")
+     << std::left;
+  (ss << ... << (x));
+  ss << '\n';
+
+  std::cout << ss.str();
+};
+
+template <typename T, typename... Ts>
+void format_(
+    std::stringstream &ss,
+    std::string &format_template,
+    size_t& offset,
+    T &&arg,
+    Ts &&... args)
+{
+  auto start = format_template.find('{', offset);
+  auto end = format_template.find('}', start);
+  if (auto has_variable =
+          start != std::string::npos && end != std::string::npos;
+      has_variable)
+  {
+    ss << format_template.substr(offset + 1, start - (offset + 1));
+    ss << std::to_string(arg);
+    format_(ss, format_template, end, args...);
+  }
+  offset = end;
+}
+
+template <typename T>
+void format_(
+    std::stringstream &ss,
+    std::string &format_template,
+    size_t& offset,
+    T &&arg)
+{
+  auto start = format_template.find('{', offset);
+  auto end = format_template.find('}', start);
+  if (auto has_variable =
+          start != std::string::npos && end != std::string::npos;
+      has_variable)
+  {
+    ss << format_template.substr(offset + 1, start - (offset + 1));
+    ss << std::to_string(arg);
+  }
+  offset = end;
+}
+
+
+template <typename T, typename... Ts>
+std::string format(std::string format_template, T &&arg, Ts &&... args)
+{
+  std::stringstream ss;
+  auto start = format_template.find('{');
+  auto end = format_template.find('}', start);
+
+  if (auto has_variable =
+          start != std::string::npos && end != std::string::npos;
+      has_variable)
+  {
+    ss << format_template.substr(0, start);
+    ss << std::to_string(arg);
+    format_(ss, format_template, end, args...);
+    ss << format_template.substr(end + 1, format_template.size());
+  }
+  else
+  {
+    ss << format_template;
+  }
+  return ss.str();
+}
+} // namespace ns
--- nightshade/nightshade.cpp
+++ nightshade/nightshade.cpp
@@ -4,34 +4,30 @@
 #pragma comment(lib, "Dxva2.lib")
 #pragma comment(lib, "powrprof.lib")
 
-
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include <winioctl.h>
+#include <chrono>
+#include <condition_variable>
+#include <highlevelmonitorconfigurationapi.h>
+#include <iomanip>
+#include <iostream>
+#include <lowlevelmonitorconfigurationapi.h>
+#include <mutex>
 #include <ntddvdeo.h>
 #include <physicalmonitorenumerationapi.h>
-#include <highlevelmonitorconfigurationapi.h>
-#include <lowlevelmonitorconfigurationapi.h>
 #include <powrprof.h>
-#include <vector>
+#include <sstream>
 #include <thread>
-#include <chrono>
 #include <time.h>
-#include <iomanip>
-#include <condition_variable>
+#include <vector>
+#include <winioctl.h>
+
+#include "logging.h"
 #include "nightshade.h"
 
-#define log(x) \
-{ \
-time_t  t = std::time(0); \
-std::cout << ((std::ostringstream() << std::setw(10) << std::put_time(std::localtime(&t), "%H-%M-%S: ") \
-<< std::left << x << "\n").str().c_str());\
-} \
-
-using namespace std;
-using boost::asio::ip::udp;
+using udp = boost::asio::ip::udp;
 
 HANDLE timer = CreateWaitableTimer(nullptr, true, nullptr);
 
@@ -46,237 +42,407 @@ boost::system::error_code error;
 
 udp::socket udp_socket(io_context);
 
-boost::array<char, 128> recv_buf;
+boost::array<char, 128> recieve_buffer;
 
 std::chrono::system_clock::time_point stime;
 
-auto remote_endpoint = boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::broadcast(), 4000);
-auto local_endpoint = boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::any(), 4000);
+auto remote_endpoint = boost::asio::ip::udp::endpoint(
+    boost::asio::ip::address_v4::broadcast(), 4000);
+auto local_endpoint =
+    boost::asio::ip::udp::endpoint(boost::asio::ip::address_v4::any(), 4000);
 
-std::atomic<bool> found_heart = false;
+std::atomic<bool> synchronized = false;
 std::atomic<bool> suspended = false;
 
 void process_heartbeat(
-	const boost::system::error_code& error, // Result of operation.
-	std::size_t bytes_transferred           // Number of bytes received.
+    const boost::system::error_code &error, // Result of operation.
+    std::size_t bytes_transferred           // Number of bytes received.
 )
 {
-	auto rtime = *(std::chrono::system_clock::time_point*)(recv_buf.data());
-	if (bytes_transferred && rtime != stime) {
-		found_heart = true;
-		log("found heartbeat");
-	}
-	else {
-		log("heard own heartbeat")
-			return udp_socket.async_receive_from(
-				boost::asio::buffer(recv_buf), local_endpoint, process_heartbeat);
-	}
+  ns::log(__func__);
+  auto rtime =
+      *(std::chrono::system_clock::time_point *)(recieve_buffer.data());
+  if (bytes_transferred && rtime != stime)
+  {
+    synchronized = true;
+    ns::log("found heartbeat");
+  }
+  else
+  {
+    ns::log("heard own heartbeat");
+    return udp_socket.async_receive_from(
+        boost::asio::buffer(recieve_buffer), local_endpoint, process_heartbeat);
+  }
 }
 
-void send_heartbeat()
+void broadcast_synchronization_message()
 {
-	log("send_hearbeat");
-	found_heart = false;
-	::stime = std::chrono::system_clock::now();
-	boost::array<std::chrono::system_clock::time_point, sizeof(::stime)> send_buf = { ::stime };
-	udp_socket.send_to(boost::asio::buffer(send_buf), remote_endpoint);
+  ns::log(__func__);
+  synchronized = false;
+  ::stime = std::chrono::system_clock::now();
+  boost::array<std::chrono::system_clock::time_point, sizeof(::stime)>
+      send_buf = {::stime};
+  udp_socket.send_to(boost::asio::buffer(send_buf), remote_endpoint);
 }
 
-auto darken_screens(HANDLE& h, HWND& hWnd, HWND& hShell, bool supports_hw_power_off, DWORD min_brightness) {
-	log("darken");
-
-	DISPLAY_BRIGHTNESS _displayBrightness{
-		DISPLAYPOLICY_BOTH,	min_brightness, min_brightness
-	};
-
-	DWORD nOutBufferSize = sizeof(_displayBrightness);
-	DWORD ret = NULL;
-
-	if (supports_hw_power_off)
-	{
-		log("turn off display power");
-		SetVCPFeature(h, 0xD6, 0x04); // turn off display
-		return false;
-	}
-	else if (DeviceIoControl(h, IOCTL_VIDEO_SET_DISPLAY_BRIGHTNESS,
-		(DISPLAY_BRIGHTNESS*)&_displayBrightness,
-		nOutBufferSize, NULL, 0, &ret, NULL))
-	{
-		log("lower brightness & suspend");
-
-		if (!timer)throw;
-
-		LARGE_INTEGER span;
-		auto minutes = [](long long x) { return -x * 60 * 10'000'000ll; };
-		span.QuadPart = minutes(4);
-
-		if (!SetWaitableTimer(timer, &span, 0, nullptr, nullptr, true)) throw;
-		if (GetLastError() == ERROR_NOT_SUPPORTED) throw;
-
-		auto enter_sleep = []() {
-			log("wait on timer");
-			if (WaitForSingleObject(timer, INFINITE) != WAIT_OBJECT_0)
-			{
-				log("WaitForSingleObject failed (%d)\n");
-				log(GetLastError());
-			}
-			else log("Timer was signaled.\n");
-
-			SetThreadExecutionState(ES_CONTINUOUS);
-			suspended = false;
-			log("awaken");
-		};
-
-		std::thread(enter_sleep).detach();
-
-		if (!SetThreadExecutionState(ES_CONTINUOUS | ES_AWAYMODE_REQUIRED)) throw;
-		suspended = true;
-		SetSuspendState(false, false, false);
-		return true;
-	}
-	return false;
+enum class DARKEN_RESULT : bool
+{
+  POWER_OFF_MONITORS = false,
+  SUSPEND_OS = true
 };
 
-auto brighten_screens(HANDLE& h, HWND& hWnd, HWND& hShell, bool supports_hw_power_off, DWORD max_brightness) {
-	DISPLAY_BRIGHTNESS _displayBrightness{
-	DISPLAYPOLICY_BOTH,
-	max_brightness,
-	max_brightness
-	};
+auto darken_screens(
+    std::vector<HANDLE> &monitors,
+    HWND &hWnd,
+    HWND &hShell,
+    bool supports_hw_power_off,
+    DWORD min_brightness)
+{
+  ns::log(__func__);
 
+  DISPLAY_BRIGHTNESS _displayBrightness{
+      DISPLAYPOLICY_BOTH, min_brightness, min_brightness};
 
-	log("lighten");
-	SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+  DWORD nOutBufferSize = sizeof(_displayBrightness);
+  DWORD ret = NULL;
 
-	DWORD nOutBufferSize = sizeof(_displayBrightness);
+  // Screen darkening happens two ways:
+  // (1) turn off monitor power, or
+  // (2) suspend the OS
 
-	DWORD ret = NULL;
-	if (supports_hw_power_off)
-	{
-		SetVCPFeature(h, 0xD6, 0x01); // turn on display
-	}
-	else if (DeviceIoControl(h, IOCTL_VIDEO_SET_DISPLAY_BRIGHTNESS,
-		(DISPLAY_BRIGHTNESS*)& _displayBrightness,
-		nOutBufferSize, NULL, 0, &ret, NULL))
-	{
-		SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, -1); // turn on display
-	}
-	return true;
+  bool suspend = !supports_hw_power_off;
+
+  if (supports_hw_power_off)
+  {
+    for (auto monitor : monitors)
+    {
+      if (supports_hw_power_off)
+      {
+        //# Turn the monitor off.
+        SetVCPFeature(monitor, 0xD6, 0x04);
+      }
+      else if (!DeviceIoControl(
+                   monitor,
+                   IOCTL_VIDEO_SET_DISPLAY_BRIGHTNESS,
+                   (DISPLAY_BRIGHTNESS *)&_displayBrightness,
+                   nOutBufferSize,
+                   NULL,
+                   0,
+                   &ret,
+                   NULL))
+      {
+        throw;
+      }
+    }
+
+    return DARKEN_RESULT::POWER_OFF_MONITORS;
+  }
+  else
+  {
+    auto minutes = [](long long x) { return -x * 60 * 10'000'000ll; };
+
+    LARGE_INTEGER duration;
+    duration.QuadPart = minutes(4);
+
+    if (constexpr auto RUN_ONCE = 0;
+        !SetWaitableTimer(timer, &duration, RUN_ONCE, nullptr, nullptr, true))
+    {
+      throw;
+    }
+    if (GetLastError() == ERROR_NOT_SUPPORTED)
+    {
+      throw;
+    }
+
+    auto wakeup_handler = []() {
+      ns::log(__func__);
+      if (WaitForSingleObject(timer, INFINITE) != WAIT_OBJECT_0)
+      {
+        throw;
+      }
+
+      // Wake up laptop monitors
+      if (!SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED))
+      {
+        throw;
+      }
+      suspended = false;
+    };
+
+    // Create a new process to handle wake-up event so this thread can call
+    // SetSuspendState.
+    std::thread(wakeup_handler).detach();
+
+    // Tell OS we need to keep executing in suspended state; attempts to avoid
+    // entering deeper sleep states.
+    if (!SetThreadExecutionState(ES_CONTINUOUS | ES_AWAYMODE_REQUIRED))
+      throw;
+
+    suspended = true;
+    SetSuspendState(false, false, false);
+
+    return DARKEN_RESULT::SUSPEND_OS;
+  }
 };
 
-auto sheduler(HANDLE h, HWND hWnd, HWND hShell, bool supports_hw_power_off, DWORD max_brightness, DWORD min_brightness)
+void brighten_screens(
+    std::vector<HANDLE> &monitors,
+    HWND &hWnd,
+    HWND &hShell,
+    bool supports_hw_power_off,
+    DWORD max_brightness)
 {
-	while (!worker_should_terminate) {
+  ns::log(__func__);
 
-		std::unique_lock<std::mutex> lock(m);
+  DISPLAY_BRIGHTNESS _displayBrightness{
+      DISPLAYPOLICY_BOTH, max_brightness, max_brightness};
 
-		SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
-		if (!found_heart) send_heartbeat();
+  // Inform OS that we want to do work during suspended state; tries to prevent
+  // entering deeper sleep states that we cannot come back from.
+  SetThreadExecutionState(
+      ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
 
-		brighten_screens(h, hWnd, hShell, supports_hw_power_off, max_brightness);
-		if (cv.wait_for(lock, std::chrono::minutes(20), []() {return worker_should_terminate; }))
-			return;
+  for (auto monitor : monitors)
+  {
+    if (supports_hw_power_off)
+    {
+      //# Turn the monitor on.
+      constexpr auto VCP_CODE_POWER_MODE = 0xD6;
+      constexpr auto ON = 0xD6;
+      SetVCPFeature(monitor, VCP_CODE_POWER_MODE, ON);
+    }
+    else if (DWORD ret = NULL, nOutBufferSize = sizeof(_displayBrightness);
+             //# Turn monitor brightness to maximum.
+             DeviceIoControl(
+                 monitor,
+                 IOCTL_VIDEO_SET_DISPLAY_BRIGHTNESS,
+                 (DISPLAY_BRIGHTNESS *)&_displayBrightness,
+                 nOutBufferSize,
+                 NULL,
+                 0,
+                 &ret,
+                 NULL))
+    {
+      //# Turn the monitor on (work-around for laptop monitors turning off when
+      // suspended).
+      constexpr auto POWER_ON = -1;
+      SendMessage(HWND_BROADCAST, WM_SYSCOMMAND, SC_MONITORPOWER, POWER_ON);
+    }
+    // else ...???
+  }
+};
 
-		// if we slept we don't call wait_for
-		auto will_suspend = darken_screens(h, hWnd, hShell, supports_hw_power_off, min_brightness);
-		if (will_suspend) {
-			log("waiting until computer wakes up")
-				cv.wait(lock, []() { return !suspended.load(); });
-		}
-		else {
-			log("waiting for sleep timer")
-				if (cv.wait_for(lock, std::chrono::minutes(4), []() {return worker_should_terminate; }))
-					return;
-		}
-	}
+auto sheduler(
+    std::vector<HANDLE> &monitors,
+    HWND hWnd,
+    HWND hShell,
+    bool supports_hw_power_off,
+    DWORD max_brightness,
+    DWORD min_brightness)
+{
+  while (!worker_should_terminate)
+  {
+    std::unique_lock<std::mutex> lock(m);
+
+    SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+
+    if (!synchronized)
+    {
+      broadcast_synchronization_message();
+    }
+
+    brighten_screens(
+        monitors, hWnd, hShell, supports_hw_power_off, max_brightness);
+
+    // wait for some time and continue -- or exit thread when signalled.
+    if (cv.wait_for(lock, std::chrono::minutes(20), []() {
+          return worker_should_terminate;
+        }))
+    {
+      return; // exit thread
+    }
+
+    // Screen can be darkened by turning off monitors or suspending the OS
+    // (entering sleep modes):
+    // - When the monitors are turned off we just wait before turning them back
+    // on.
+    // - When the OS is suspended we wait until the OS resumes.
+
+    auto result = darken_screens(
+        monitors, hWnd, hShell, supports_hw_power_off, min_brightness);
+
+    if (result == DARKEN_RESULT::SUSPEND_OS)
+    {
+      ns::log("Waiting until computer wakes up.");
+      cv.wait(lock, []() { return !suspended.load(); });
+    }
+    else
+    {
+      ns::log("Waiting for sleep timer.");
+      if (cv.wait_for(lock, std::chrono::minutes(4), []() {
+            return worker_should_terminate;
+          }))
+      {
+        return; // exit thread
+      }
+    }
+  }
 }
 
-int main(int argc, char* argv[])
+/// <summary>
+/// Collects all monitor HANDLEs
+/// </summary>
+bool query_monitors(HMONITOR Arg1, HDC Arg2, LPRECT Arg3, LPARAM Arg4)
 {
-	udp_socket.open(udp::v4(), error);
-	udp_socket.set_option(boost::asio::socket_base::broadcast(true));
-	udp_socket.set_option(boost::asio::ip::udp::socket::reuse_address(true));
-	udp_socket.bind(local_endpoint);
 
-	auto hWnd = GetConsoleWindow();
-	//ShowWindow(hWnd, SW_HIDE);
+  std::vector<HANDLE> *monitors = reinterpret_cast<std::vector<HANDLE> *>(Arg4);
+  DWORD amount;
+  GetNumberOfPhysicalMonitorsFromHMONITOR(Arg1, &amount);
 
-	auto hShell = FindWindow("Shell_TrayWnd", NULL);
-	
-	HANDLE h = CreateFile("\\\\.\\LCD",
-		GENERIC_READ | GENERIC_WRITE,
-		0,
-		NULL,
-		OPEN_EXISTING,
-		0, NULL);
+  PHYSICAL_MONITOR *pMonitors = new PHYSICAL_MONITOR[amount];
 
-	auto use_external_monitor = h == INVALID_HANDLE_VALUE;
-	if (use_external_monitor)
-	{
-		auto monitor = MonitorFromWindow(hWnd, MONITOR_DEFAULTTOPRIMARY);
-		DWORD amount;
-		GetNumberOfPhysicalMonitorsFromHMONITOR(monitor, &amount);
+  if (GetPhysicalMonitorsFromHMONITOR(Arg1, amount, pMonitors))
+  {
+    ns::log(ns::format(
+        "Found monitor #{number}: {description}",
+        monitors->size() + 1,
+        pMonitors->szPhysicalMonitorDescription));
+    monitors->emplace_back(pMonitors->hPhysicalMonitor);
 
-		PHYSICAL_MONITOR* pMonitors = new PHYSICAL_MONITOR[amount];
+    bool supports_power_off = false;
+    DWORD max;
+    if (DWORD current; supports_power_off = GetVCPFeatureAndVCPFeatureReply(
+            pMonitors->hPhysicalMonitor, 0xD6, nullptr, &current, &max))
+    {
+      ns::log("Supports power control.");
+    }
 
-		if (GetPhysicalMonitorsFromHMONITOR(monitor, amount, pMonitors))
-		{
-			h = pMonitors->hPhysicalMonitor;
-			delete[] pMonitors;
-		}
-	}
+    delete[] pMonitors;
+  }
 
-	DWORD current, max;
-	auto supports_hw_power_off = GetVCPFeatureAndVCPFeatureReply(h, 0xD6, nullptr, &current, &max);
-	
-	// Set shutdown privilege for current process
-	{ 
-		auto pid = GetCurrentProcess();
-		HANDLE token;
-		if (!OpenProcessToken(pid, TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token)) throw;
-		LUID luid;
-		LookupPrivilegeValue(nullptr, "SeShutdownPrivilege", &luid);
-		TOKEN_PRIVILEGES priv;
-		priv.PrivilegeCount = 1;
-		priv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
-		priv.Privileges[0].Luid = luid;
+  return true;
+};
 
-		if (!AdjustTokenPrivileges(token, false, &priv, 0, nullptr, nullptr)) throw;
-		CloseHandle(token);
-	}
+int main(int argc, char *argv[])
+{
 
-	auto task = std::bind(sheduler, h, hWnd, hShell, supports_hw_power_off, max, 0);
-	worker = std::thread(task);
+  udp_socket.open(udp::v4(), error);
+  udp_socket.set_option(boost::asio::socket_base::broadcast(true));
+  udp_socket.set_option(boost::asio::ip::udp::socket::reuse_address(true));
+  udp_socket.bind(local_endpoint);
 
-	auto should_shutdown = false;
-	while (!should_shutdown)
-	{
-		SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
-		if (found_heart) 
-		{
-			{
-				log("tell the worker thread to terminate.")
-				std::lock_guard<std::mutex> lock(m);
-				worker_should_terminate = true;
-				cv.notify_all();
-			}
-			worker.join();
-			log("worker thread joined main thread.")
-			{
-				worker_should_terminate = false;
-				worker = std::thread(task);
-			}
-		}
-		else
-		{
-			worker_should_terminate = false;
-		}
+  auto hWnd = GetConsoleWindow();
 
-		udp_socket.async_receive_from(
-			boost::asio::buffer(recv_buf), local_endpoint, process_heartbeat);
-		io_context.run(); // execution on this thread will poll here until a heartbeat is heard...
-		io_context.restart(); 
-	}
+  if (!timer)
+  {
+    throw;
+  }
 
-	return 0;
+  // ShowWindow(hWnd, SW_HIDE);
+  auto hShell = FindWindow("Shell_TrayWnd", NULL);
+
+  std::vector<HANDLE> monitors;
+
+  // Find handles for monitors.
+  {
+    // Check if there is a laptop monitor.
+    HANDLE laptop_monitor = CreateFile(
+        "\\\\.\\LCD",
+        GENERIC_READ | GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL);
+
+    auto use_external_monitor = laptop_monitor == INVALID_HANDLE_VALUE;
+
+    if (use_external_monitor)
+    {
+      EnumDisplayMonitors(
+          nullptr,
+          nullptr,
+          MONITORENUMPROC(&query_monitors),
+          reinterpret_cast<LPARAM>(&monitors));
+    }
+    else
+    {
+      monitors.push_back(laptop_monitor);
+    }
+  }
+
+  bool supports_power_off = false;
+  DWORD max;
+  if (DWORD current; supports_power_off = GetVCPFeatureAndVCPFeatureReply(
+                         monitors[1], 0xD6, nullptr, &current, &max))
+  {
+    ns::log("Supports power control.");
+  }
+
+  // Set the shutdown privilege for this process.
+  {
+    if (HANDLE token; OpenProcessToken(
+            GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &token))
+    {
+      LUID luid;
+      LookupPrivilegeValue(nullptr, SE_SHUTDOWN_NAME, &luid);
+
+      TOKEN_PRIVILEGES priv;
+      priv.PrivilegeCount = 1;
+      priv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+      priv.Privileges[0].Luid = luid;
+
+      if (!AdjustTokenPrivileges(token, false, &priv, 0, nullptr, nullptr))
+        throw; // throw on failure
+
+      CloseHandle(token);
+    }
+    else
+    {
+      throw; // throw on failure
+    }
+  }
+
+  auto pomodoro_timer =
+      std::bind(sheduler, monitors, hWnd, hShell, supports_power_off, max, 0);
+  worker = std::thread(pomodoro_timer);
+
+  auto should_shutdown = false;
+  while (!should_shutdown)
+  {
+
+    SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+
+    if (synchronized)
+    {
+      // Restart the worker to synchronize heartbeats
+      {
+        ns::log("Terminate the worker thread.");
+        std::lock_guard<std::mutex> lock(m);
+        worker_should_terminate = true;
+        cv.notify_all();
+      }
+      worker.join();
+      ns::log("Worker thread has joined main thread.");
+      {
+        // Start new worker
+        worker_should_terminate = false;
+        worker = std::thread(pomodoro_timer);
+      }
+    }
+    else
+    {
+      worker_should_terminate = false;
+    }
+
+    // Listen for a heartbeat over the network.
+
+    udp_socket.async_receive_from(
+        boost::asio::buffer(recieve_buffer), local_endpoint, process_heartbeat);
+    io_context.run(); // execution on this thread will poll here until a
+                      // heartbeat is heard...
+    io_context.restart();
+  }
+
+  return 0;
 }
